7d80f47e6190df9cd0446864b5d0de59c8add353
verzyo <279150665+verzyo@users.noreply.github.com>
2026-07-12 22:41:43 +0200

    feat: add home-manager flake input

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
