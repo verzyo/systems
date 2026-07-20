debfbe5aa7e76303c733179f63906740362d3e20
verzyo <279150665+verzyo@users.noreply.github.com>
2026-07-20 19:30:50 +0200

    feat: add auto discovery of hosts

--- parts/nixosConfigurations.nix
+++ parts/nixosConfigurations.nix
@@ -1,16 +1,23 @@
 {
   inputs,
+  lib,
   self,
   ...
-}: {
-  flake.nixosConfigurations = {
-    laptop = inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        "${self}/hosts/laptop/"
+}: let
+  hostnames = builtins.attrNames (
+    lib.filterAttrs (_: type: type == "directory")
+    (builtins.readDir "${self}/hosts")
+  );
+in {
+  flake.nixosConfigurations = lib.genAttrs hostnames (
+    hostname:
+      inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
 
-        (inputs.import-tree "${self}/modules/nixos")
-      ];
-    };
-  };
+        modules = [
+          "${self}/hosts/${hostname}"
+          (inputs.import-tree "${self}/modules/nixos")
+        ];
+      }
+  );
 }
