2e878a7d86a2304c907a80734bfaf05ab6749a8e
verzyo <279150665+verzyo@users.noreply.github.com>
2026-07-20 16:31:36 +0200

    feat: add detect-private-keys to flake check

--- parts/checks.nix
+++ parts/checks.nix
@@ -8,6 +8,7 @@
         package = config.treefmt.build.wrapper;
       };
 
+      detect-private-keys.enable = true;
       check-merge-conflicts.enable = true;
       trim-trailing-whitespace.enable = true;
       end-of-file-fixer.enable = true;
