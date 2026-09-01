c6ca0f1415487d1c734092a332d02d61a1b368dc
verzyo <279150665+verzyo@users.noreply.github.com>
2026-09-01 16:05:41 +0000

    feat: add cliproxyapi plugins declaratively

--- modules/home/services/cliproxyapi.nix
+++ modules/home/services/cliproxyapi.nix
@@ -12,9 +12,29 @@
       enable = true;
       managementPasswordFile = lib.mkIf osConfig.modules.sops.enable osConfig.sops.secrets."management_key".path;
 
+      plugins = [
+        "antigravity-coding-filter"
+        "antigravity-priority"
+        "quota-activation"
+        "privacyfilter"
+        "cpa-apply-patch"
+      ];
+
       settings = {
         host = "localhost";
         port = 8317;
+
+        plugins = {
+          enabled = true;
+
+          configs = {
+            "antigravity-coding-filter".enabled = true;
+            "antigravity-priority".enabled = true;
+            "cpa-apply-patch".enabled = true;
+            "privacyfilter".enabled = true;
+            "quota-activation".enabled = true;
+          };
+        };
 
         remote-management.disable-control-panel = false;
       };
